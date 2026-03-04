fafece1e3df7cf422639124c579212b4adfc9c0b
JRsuppanut <128184040+JRsuppanut@users.noreply.github.com>
2026-03-05 05:26:52 +0700

    add csv file loader
    add csv file loader
    and edit class Course

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
-import heapq
 import csv
 
 class Course:
-    def __init__(self , code , name , type , credit  , semester , lecturer):
+    def __init__(self , code , name , c_type , credit  , semester , lecturer):
         self.code = code
         self.name = name
-        self.type = type
+        self.c_type = c_type
         self.credit = credit
         self.semester = semester
         self.lecturer = lecturer
 
-        self.undo.activate = True
+        self.isActivate = True
 
     def __str__(self):
-        return f"{self.code} | {self.name} | {self.type} | {self.credit} | {self.semester} | {self.lecturer}"
-    
+        return f"{self.code:<10} | {self.name:<35} | {self.c_type:<5} | {self.credit:<8} | {self.semester:<3} | {self.lecturer}"
+    __repr__ = __str__
 
+def loadCourse(filename):
+    allCourse = []
+    with open(filename , mode='r', encoding='utf-8-sig' ) as file:
+        reader = csv.reader(file)
+        next(reader) #skip the 1st line (it's not data)
+
+        for row in reader:
+            course_object = Course(
+                code=row[0].strip(),
+                name=row[1].strip(),
+                c_type=row[2].strip(),
+                credit=row[3].strip(),
+                semester=row[4].strip(),
+                lecturer=row[5].strip())
+
+            allCourse.append(course_object)
+    
+    return allCourse
+
+
+myCourse = loadCourse('CprE_Subject.csv')
+print(f"length : {len(myCourse)}")
+print(*myCourse[:5], sep='\n')
